e1e5f9e302c232d306e3410ea591068ecce28585
CarlosMH95 <cmanosalvas95@gmail.com>
2017-08-27 19:25:21 -0500

    Commit Inicial

--- persona/tests.py
+++ persona/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
+from persona.models import Persona
+from persona.views import listarPersonas, detallesPersona
 
 # Create your tests here.
+def PruebaCajaBlanca(TestCase):
+    def setup(self):
+        persona = Persona()
+        persona.nombre = "Carlos"
+        persona.apellido = "Manosalvas"
+        persona.cedula = "03254698745"
+        persona.direccion = "Sauces 3"
+        persona.ocupacion = "Estudiante"
+        persona.save()
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@…', password='top_secret')
+    def test_1(self):
+        request = self.factory.get('/persona/listar')
+
+        response = listarPersonas(request)
+        self.assertEqual(response.status_code, 200)
